92b339c883075a69f8260bd77f8c848739c83d3a
Mateusz Nejman <mateusz_nejman@hotmail.com>
2019-05-23 14:29:34 +0200

    Add files via upload

---
+++ CryptoString.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace Nejman.CSEC
+{
+    public class CryptoString
+    {
+        private byte[] GetKey(string pass)
+        {
+            byte[] key = new byte[16];
+
+            for (int a = 0; a < key.Length; a++)
+            {
+                if (a < pass.Length)
+                    key[a] = (byte)pass[a];
+                else
+                    key[a] = (byte)a;
+            }
+
+
+            return key;
+        }
+
+        public byte[] Encrypt(string text, string pass)
+        {
+            RijndaelManaged rm = new RijndaelManaged();
+            MemoryStream ms = new MemoryStream();
+            CryptoStream cs = new CryptoStream(ms, rm.CreateEncryptor(GetKey(pass), GetKey(pass)), CryptoStreamMode.Write);
+            StreamWriter sw = new StreamWriter(cs);
+            sw.Write(text);
+            sw.Close();
+            cs.Close();
+            byte[] crypted = ms.ToArray();
+            ms.Close();
+            return crypted;
+        }
+
+        public string Decrypt(byte[] data, string pass)
+        {
+            RijndaelManaged rm = new RijndaelManaged();
+            MemoryStream ms = new MemoryStream(data);
+            CryptoStream cs = new CryptoStream(ms, rm.CreateEncryptor(GetKey(pass), GetKey(pass)), CryptoStreamMode.Read);
+            StreamReader sr = new StreamReader(cs);
+            string text = sr.ReadToEnd();
+            sr.Close();
+            cs.Close();
+            ms.Close();
+            return text;
+        }
+    }
+}
